249ce15adc18a03b07c2d23a5cd15ec7437ddd23
ChunYu Shi <shicyu@icloud.com>
2023-01-30 16:21:47 -0500

    detect

--- src/script.js
+++ src/script.js
@@ -1,8 +1,69 @@
 async function insertTableRow(emailAddress) {
-    if (!emailAddress) {
-        return
-    }
+  if (!emailAddress) {
+    return;
+  }
 
-    /* Table Being used is on page: https://coda.io/d/_dIn5QMLAoCF/Landing-Page-Sign-Ups_suucR */
-    const endpoint = new URL(`https://coda.io/apis/v1/docs/In5QMLAoCF/tables/grid-EWkwkvROeO/rows`);
+  /* Table Being used is on page: https://coda.io/d/_dIn5QMLAoCF/Landing-Page-Sign-Ups_suucR */
+  const endpoint = new URL(
+    `https://coda.io/apis/v1/docs/In5QMLAoCF/tables/grid-EWkwkvROeO/rows`
+  );
+}
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'Darwin'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'MacOS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+function downloadURI(uri, name) {
+  var link = document.createElement('a');
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
+function downloadApp() {
+  const detectedOS = getOS();
+
+  if (
+    (detectedOS === null) |
+    (detectedOS === 'Linux') |
+    (detectedOS === 'iOS') |
+    (detectedOS === 'Android')
+  ) {
+    scrollTo('sign-up-anchor');
+  } else if (detectedOS === 'MacOS') {
+    // pop-up
+  } else if (detectedOS === 'Windows') {
+    downloadURI(
+      'assets/apps/Remembered-0.1.0-x64.dmg',
+      'Remembered-0.1.0-x64.dmg'
+    );
+  }
+
+  console.log('detectedOS:', detectedOS);
+}
+
+function scrollTo(hash) {
+  location.hash = '#' + hash;
 }
